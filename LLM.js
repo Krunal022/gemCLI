5b3928e4bb701213115f1edfbb93f2efb09b77c6
Krunal022 <krunalvaghela723@gmail.com>
2025-06-20 13:08:25 +0530

    Setup dotenv for secure API key management

--- LLM.js
+++ LLM.js
@@ -1,11 +1,13 @@
 import { GoogleGenAI } from "@google/genai";
+import dotenv from 'dotenv';
+dotenv.config();
 
-const ai = new GoogleGenAI({ apiKey: "AIzaSyBNpOjuZJNqBA07lUCwwwCdbxkr38INkDc" });
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 async function main() {
     const response = await ai.models.generateContent({
         model: "gemini-2.5-flash",
-        contents: "Hi, my name is Krunal.",
+        contents: "What is array?",
     });
     console.log("")
     console.log("response-->");
